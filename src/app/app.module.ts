7a5bd1c474edc38cd7a4361b4845aeff083bf9c4
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-08-27 22:45:46 +0200

    Added baseline structure for atf
    The static atf section is formatted and styled. Portrait image still needs to be replaced with highe resolution and size.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AtfComponent } from './atf/atf.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AtfComponent,  
+    AtfComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    NgIf
   ],
   providers: [],
   bootstrap: [AppComponent]
