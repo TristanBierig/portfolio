5386031ec0fa35cc0b2787a7340cc0614053bede
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-08-27 18:48:37 +0200

    Add components ATF & Nav
    Basic angular structure for the first Section (ATF). Also added first html and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AtfComponent } from './atf/atf.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AtfComponent,  
   ],
   imports: [
     BrowserModule,
