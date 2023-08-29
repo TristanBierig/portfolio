ecf20ab1ae2857cdf844637029324b295c83908d
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-08-29 14:43:58 +0200

    Add Text

---
+++ src/app/preamble/preamble.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preamble',
+  templateUrl: './preamble.component.html',
+  styleUrls: ['./preamble.component.scss']
+})
+export class PreambleComponent {
+
+}
