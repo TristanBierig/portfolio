9d5d2c01d0de815c670627f54182d8f5b0b55f1a
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-09-07 22:23:53 +0200

    Created Service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SidenavService } from '../sidenav.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   public showFiller: boolean = false;
   public showSidenav: boolean = false;
- 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor(private _sidenavService: SidenavService) {}
 
+  ngOnInit() {}
 
   toggleSidenav() {
     this.showSidenav = !this.showSidenav;
   }
-
 }
