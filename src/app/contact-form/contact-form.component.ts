db41e47f885a8fdf9986f8abd4db7fec34d0a36e
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-09-08 17:24:57 +0200

    Validation works
    Reset still does not reset input touched, dirty states, so it will display the error messages for required inputs on submit. Else validation works

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,20 +1,58 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  styleUrls: ['./contact-form.component.scss'],
 })
-export class ContactFormComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+export class ContactFormComponent implements OnInit {
+  contactForm!: FormGroup;
+  // name: FormControl;
+  // email: FormControl;
+  // message: FormControl;
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.contactForm = new FormGroup({
+      name: this.nameControl,
+      email: this.emailControl,
+      message: this.messageControl,
+    });
+  }
+
+  nameControl = new FormControl('', Validators.required);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  messageControl = new FormControl('', Validators.required);
+
+  getErrorMessageEmail() {
+    if (this.emailControl.hasError('required')) {
+      return 'Enter an email to be answered to';
     }
+    return this.emailControl.hasError('email') ? 'Not a valid email' : '';
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  getErrorMessageText() {
+    if (this.messageControl.hasError('required')) {
+      return 'Please enter your message';
+    }
+    return;
+  }
+
+  getErrorMessageName() {
+    if (this.emailControl.hasError('required')) {
+      return 'Please enter your name';
+    }
+    return;
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted');
+      this.contactForm.reset();
+    }
   }
 }
